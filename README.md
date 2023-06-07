e07baabcae356ec5145c6b1f931d8aa7c2e533ad
Tb987 <131566940+Tb987@users.noreply.github.com>
2023-06-07 15:11:11 -0700

    made readMe

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Password-genorator
+Following the password instuction, a passwork will be generated. 
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
